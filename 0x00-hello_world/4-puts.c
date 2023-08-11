381bd42b8289c553c09519ad72943e27c21782d3
Chizoba Samuel Aboviekie <chizzwhite63@gmail.com>
2023-08-11 04:36:43 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+3 * main - prints the phrase "Programming is like building a multilingual puzzle
+5 * Return: 0 if exited properly,non-zero otherwise
+6 */
+int main(void) 
+{
+ puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}   
