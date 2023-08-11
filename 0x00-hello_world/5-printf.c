3d31a87f31180e4dd607bc0b284d168905ef7b14
Chizoba Samuel Aboviekie <chizzwhite63@gmail.com>
2023-08-11 09:19:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: 0 if exited properly,non-zero otherwise
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-     printf("with proper grammer, but the outcome is a piece of art,\n");
-     return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
