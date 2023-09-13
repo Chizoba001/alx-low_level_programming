ed093b0eb3871c5b81a46eb22214d892f765efdc
Chizoba Samuel Aboviekie <chizzwhite63@gmail.com>
2023-09-13 15:36:13 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,7 +3,7 @@
 /**
  * int_index - earches for an integer
  * @array: array to search in
- * @size: size of array
+ * @size: size of the array
  * @cmp: pointer to the comparing function
  *
  * Return: index of the first element for which
@@ -16,11 +16,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp)
 	{
-		for (i = 0; i < size, i++)
+		for (i = 0; i < size; i++)
 		{
-			if {cmp(array[i]) != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
+
 	return (-1);
 }
