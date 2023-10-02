adcbae207ef03f760e701e57722ef6270b41d763
Chizoba Samuel Aboviekie <chizzwhite63@gmail.com>
2023-10-02 15:36:22 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,35 +14,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-	FILE *file;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
 	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-	return (0);
+		close(fd);
+		return (0);
 	}
-
-	file = fopen(filename, "r");
-
-	char buffer[letters + 1];
-
-	ssize_t bytes_read = fread(buffer, sizeof(char), letters, file);
-
-	fclose(file);
-
-	ssize_t bytes_written;
-
-	bytes_written = fwrite(buffer, sizeof(char), bytes_read, stdout);
-
-	if (bytes_read <= 0)
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
-	return (0);
+		free(buffer);
+		return (0);
 	}
-
-	if (bytes_written != bytes_read)
-	{
-	return (0);
-	}
-
-	return (bytes_read);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
