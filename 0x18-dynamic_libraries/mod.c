b348f7c3a9294dc71fe97961b7f5dfe2582216c2
Chizoba001 <chizzwhite63@gmail.com>
2023-12-31 09:15:27 -0800

    6-hash_table_delete.c

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * mod - mod two integers
+ * @a: first unit
+ * @b: second int
+ * Return:multiply
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
